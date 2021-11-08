034b97f14eb15dfe00911d2d9d50ed76faa61f07
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2021-11-08 12:55:09 +0800

    feat: Added of the Profile description page & SpecFlow Implementation

---
+++ MarsOnBoardingTask/MarsOnBoardingTask/SpecFlow/StepDefinition/ProfileDescriptionSteps.cs
@@ -0,0 +1,66 @@
+﻿using MarsOnBoardingTask.Pages;
+using MarsOnBoardingTask.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MarsOnBoardingTask.SpecFlow.StepDefinitions
+{
+    [Binding]
+    public class ProfileDescriptionSteps : Hooks
+    {
+        // Login Page object initialization and definition
+        LoginPage loginPageObj = new LoginPage();
+
+        // Login Page object initialization and definition
+        ProfileDescription profileDetailsObj = new ProfileDescription();
+
+        [Given(@"I login to the Trade Skills portal description successfully")]
+        public void GivenILoginToTheTradeSkillsPortalDescriptionSuccessfully()
+        {
+            // Open chrome browser
+            testDriver = new ChromeDriver();
+            loginPageObj.LoginPageSteps(testDriver);
+        }
+
+        [Given(@"I click on the description pen icon")]
+        public void GivenIClickOnTheDescriptionPenIcon()
+        {
+            profileDetailsObj.DescriptionPenIcon(testDriver);
+        }
+
+        [When(@"I add the '(.*)' and click on Save button")]
+        public void WhenIAddTheAndClickOnSaveButton(string Description)
+        {
+            profileDetailsObj.AddDescription(testDriver, Description);
+        }
+
+        [Then(@"the Description should be added as '(.*)' successfully")]
+        public void ThenTheDescriptionShouldBeAddedAsSuccessfully(string Description)
+        {
+            // Assertion that Description record has been created.
+            Assert.AreEqual(profileDetailsObj.GetDescription1(testDriver), Description, "Actual Description and expected Description don't match");
+        }
+
+        [When(@"I edit the '(.*)' and click on Save button")]
+        public void WhenIEditTheAndClickOnSaveButton(string Description)
+        {
+            profileDetailsObj.EditDescription(testDriver, Description);
+        }
+
+        [Then(@"the Description should be updated as '(.*)' successfully")]
+        public void ThenTheDescriptionShouldBeUpdatedAsSuccessfully(string Description)
+        {
+            // Assertion that Description record has been edited.
+            Assert.AreEqual(profileDetailsObj.GetDescription2(testDriver), Description, "Actual Description and expected Description don't match");
+        }
+
+
+
+
+
+
+
+    }
+}
