63df8ce2ab556e47eb4b28ad8d05f8c32a261e7e
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2021-11-08 12:40:30 +0800

    feat: Added wait implementation and the dependencies

---
+++ MarsOnBoardingTask/MarsOnBoardingTask/Utils/ConstantHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MarsOnBoardingTask.Utils
+{
+    class ConstantHelpers
+    {
+        //Base Url
+        public static string BaseUrl = "http://localhost:5000/";
+
+        //ScreenshotPath
+        public static string ScreenshotPath = "@/MarsOnBoardingTask/TestReports/Screenshots/";
+
+        //ExtentReportsPath
+        public static string ReportsPath = "@/MarsOnBoardingTask/TestReports/Reports";
+
+        //ReportXML Path
+        public static string ReportXMLPath = "";
+    }
+}
